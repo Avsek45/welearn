4bc0a972f8503c5dd137a29095f2918f5a32552d
Abhishek Maharjan <abhishek.maharjan@pluto.tv>
2023-04-01 00:00:24 -0700

    fixed key naming

--- tf/services/ec2.tf
+++ tf/services/ec2.tf
@@ -1,10 +1,12 @@
-module "ec2_instances" {
-  source         = "../modules/ec2-instaces/"
-  ami_id         = "ami-0123456789abcdef0"
-  instance_type  = "t2.micro"
-  key_name       = "ouraws-keypair"
-  instance_count = 3
-
+module "ec2" {
+  source          = "../modules/ec2-instaces/"
+  ami_id          = "ami-00c39f71452c08778"
+  instance_type   = "t2.micro"
+  keypair_name    = "welearn.pem"
+  instance_count  = 3
+  region          = "us-east-1"
+  bucket_name     = "welearnkeys-nepal"
+  public_key_path = "/home/cloud_user/.ssh/id_rsa.pub"
   instance_tags = {
     Name        = "Example Instance"
     Environment = "Development"
